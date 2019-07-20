4e976e14c50c2082307922defacc71b4a1bca319
w-zengtao <so.zengtao@gmail.com>
2019-07-20 23:41:54 +0800

    [ADD] CurrentPage helper & Paginate helper

--- controllers/helper/page.go
+++ controllers/helper/page.go
@@ -1,6 +1,11 @@
 package helper
 
-import "github.com/gin-gonic/gin"
+import (
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+)
 
 /*CurrentPage returns the current paginate page number, returns 1 as default
  */
@@ -8,6 +13,20 @@ func CurrentPage(ctx *gin.Context) int {
 	return currentPage(ctx)
 }
 
+// where create query. use GET method and put page into query params
 func currentPage(ctx *gin.Context) int {
-	return 1
+	if p, ok := ctx.GetQuery("page"); ok {
+		page, _ := strconv.Atoi(p)
+		return page
+	}
+	return 1 // Set Default
+}
+
+// Page - paginate method for cnotroller use
+func Page(db *gorm.DB, pageNum int) *gorm.DB {
+	return page(db, pageNum)
+}
+
+func page(db *gorm.DB, pageNum int) *gorm.DB {
+	return db.Offset((pageNum - 1) * 10)
 }
