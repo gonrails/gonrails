584fb74c88811c3c1b724c17f4a90f83d2f220a8
曾涛 <taozeng@mindhand.io>
2020-06-30 00:34:54 +0800

    准备迁移命令行工具到  Cli 上面。然后重新调整一下目录结构

--- cmds/new.go
+++ cmds/new.go
@@ -2,29 +2,32 @@ package cmds
 
 import (
 	"fmt"
-	"log"
-	"os"
 
-	"github.com/gonrails/gonrails/cmds/new"
+	"github.com/urfave/cli/v2"
 )
 
-// New - Creates a new gonrails project
-func New(projectName string) {
-
-	log.Printf("Creating gonrails project named %s ...", projectName)
-	_ = os.Mkdir(projectName, os.ModePerm)
-
-	for _, name := range dirs {
-		log.Println(fmt.Sprintf("Making Dir: %s/%s", projectName, name))
-		_ = os.Mkdir(fmt.Sprintf("%s/%s", projectName, name), os.ModePerm)
-	}
-
-	new.TouchMain(projectName)
-	new.TouchConfig(projectName)
-	new.TouchRoute(projectName)
-	new.TouchController(projectName)
-	new.TouchSerializer(projectName)
-	new.TouchDockerfile(projectName)
-	new.InitMod(projectName)
-	new.InitGit(projectName)
+func New(ctx *cli.Context) error {
+	fmt.Println(ctx.Args())
+	return nil
 }
+
+// New - Creates a new gonrails project
+//func New(projectName string) {
+//
+//	log.Printf("Creating gonrails project named %s ...", projectName)
+//	_ = os.Mkdir(projectName, os.ModePerm)
+//
+//	for _, name := range dirs {
+//		log.Println(fmt.Sprintf("Making Dir: %s/%s", projectName, name))
+//		_ = os.Mkdir(fmt.Sprintf("%s/%s", projectName, name), os.ModePerm)
+//	}
+//
+//	new.TouchMain(projectName)
+//	new.TouchConfig(projectName)
+//	new.TouchRoute(projectName)
+//	new.TouchController(projectName)
+//	new.TouchSerializer(projectName)
+//	new.TouchDockerfile(projectName)
+//	new.InitMod(projectName)
+//	new.InitGit(projectName)
+//}
--- serializers/tenant/admin_index_serializer.go
+++ serializers/tenant/admin_index_serializer.go
@@ -7,30 +7,30 @@ Email:  zengtao@risewinter.com
 
 package tenant
 
-import (
-	"github.com/gonrails/gonrails/models"
-	"github.com/w-zengtao/struct2json"
-)
-
-// AdminTenantIndexSerializer - GET /admin/tenants
-type AdminTenantIndexSerializer struct {
-	*BaseTenantSerializer `json:"base" noroot:"true"`
-}
-
-// Serialize makes AdminTenantIndexSerializer satisfy Serializer interface
-func (s *AdminTenantIndexSerializer) Serialize(v interface{}) map[string]interface{} {
-
-	if obj, ok := v.(*models.Tenant); ok {
-		s = &AdminTenantIndexSerializer{
-			&BaseTenantSerializer{
-				ID:     obj.ID,
-				Name:   obj.Name.String,
-				APIKey: obj.ApiKey,
-			},
-		}
-		ans, _ := struct2json.Convert(s)
-		return ans
-	}
-
-	return map[string]interface{}{"error": "params passed to method Serialize(v interface{}) is not a Tenant"}
-}
+//import (
+//	"github.com/gonrails/gonrails/models"
+//	"github.com/w-zengtao/struct2json"
+//)
+//
+//// AdminTenantIndexSerializer - GET /admin/tenants
+//type AdminTenantIndexSerializer struct {
+//	*BaseTenantSerializer `json:"base" noroot:"true"`
+//}
+//
+//// Serialize makes AdminTenantIndexSerializer satisfy Serializer interface
+//func (s *AdminTenantIndexSerializer) Serialize(v interface{}) map[string]interface{} {
+//
+//	if obj, ok := v.(*models.Tenant); ok {
+//		s = &AdminTenantIndexSerializer{
+//			&BaseTenantSerializer{
+//				ID:     obj.ID,
+//				Name:   obj.Name.String,
+//				APIKey: obj.ApiKey,
+//			},
+//		}
+//		ans, _ := struct2json.Convert(s)
+//		return ans
+//	}
+//
+//	return map[string]interface{}{"error": "params passed to method Serialize(v interface{}) is not a Tenant"}
+//}
