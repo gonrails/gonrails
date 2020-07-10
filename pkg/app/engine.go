f519ea3d13e6de0b6a9942335faa2b6324018234
w-zengtao <so.zengtao@gmail.com>
2020-07-10 09:38:34 +0800

    Engine 大概的样子

--- pkg/app/engine.go
+++ pkg/app/engine.go
@@ -1,4 +1,24 @@
 package app
 
+import "gonrails/pkg/server"
+
 type Engine struct {
+	servers []server.Server
+}
+
+func (engine *Engine) initialize() {
+
+}
+
+func (engine *Engine) startServers() {
+	for _, server := range engine.servers {
+		server.Start()
+	}
+}
+
+func (engine *Engine) clean() {}
+
+func (engine *Engine) Run() {
+	defer engine.clean()
+	engine.startServers()
 }
