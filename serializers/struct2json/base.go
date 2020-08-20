d8bd2b58db3f0eca190e7f60fb3df9728f508747
w-zengtao <so.zengtao@gmail.com>
2020-08-20 23:55:17 +0800

    【移植】Struct2Json

--- serializers/struct2json/base.go
+++ serializers/struct2json/base.go
@@ -1,7 +1,79 @@
 package struct2json
 
-// Convert converts a struct `s` to a map[string]interface{}
+import (
+	"errors"
+	"reflect"
+)
+
+func getType(v interface{}) reflect.Type {
+	t := reflect.TypeOf(v)
+	if reflect.Ptr == t.Kind() {
+		t = t.Elem()
+	}
+	return t
+}
+
+func getValue(v interface{}) reflect.Value {
+	return reflect.Indirect(reflect.ValueOf(v))
+}
+
+func getKey(f *reflect.StructField) string {
+	var key = f.Name
+	if json, ok := f.Tag.Lookup("json"); ok {
+		key = json
+	}
+	return key
+}
+
+func getNoroot(f *reflect.StructField) bool {
+	if _, ok := f.Tag.Lookup("noroot"); ok {
+		return true
+	}
+	return false
+}
+
+func getMapValue(v reflect.Value) (i interface{}) {
+
+	if reflect.Ptr == v.Kind() {
+		v = v.Elem()
+	}
+
+	if reflect.Invalid == v.Kind() {
+		return nil
+	}
+
+	if reflect.Struct == v.Kind() {
+		ans, _ := Convert(v.Interface())
+		return ans
+	}
+	return v.Interface()
+}
+
+// Convert converts a struct to a common JSON
 // Convert should be recursive
-func Convert(s interface{}) (map[string]interface{}, error) {
-	return nil, nil
+func Convert(struc interface{}) (map[string]interface{}, error) {
+	ansMap := make(map[string]interface{})
+
+	sType := getType(struc)
+	sVal := getValue(struc)
+
+	if reflect.Struct != sType.Kind() {
+		return nil, errors.New("v is not a struct or a pointer to a struct")
+	}
+
+	for i := 0; i < sType.NumField(); i++ {
+		filed := sType.Field(i)
+		value := sVal.FieldByName(filed.Name)
+
+		if getNoroot(&filed) {
+			subMap := getMapValue(value).(map[string]interface{})
+			for k, v := range subMap {
+				ansMap[k] = v
+			}
+		} else {
+			ansMap[getKey(&filed)] = getMapValue(value)
+		}
+	}
+
+	return ansMap, nil
 }
